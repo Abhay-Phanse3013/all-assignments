55e3277c39f43351ddbebda2c104a84e7b90500d
Abhay Phanse <52242762+Abhay-Phanse3013@users.noreply.github.com>
2023-06-30 16:57:36 +0530

    TODO_app_running

--- week-2/Week-2-Assignments/02-nodejs/solutions/todoServer.solution.file.js
+++ week-2/Week-2-Assignments/02-nodejs/solutions/todoServer.solution.file.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require("fs");
-
+const path=require('path');
 const app = express();
+const cors= require('cors');
 
 app.use(bodyParser.json());
-
+app.use(cors());
 function findIndex(arr, id) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i].id === id) return i;
@@ -98,9 +99,13 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
-// for all other routes, return 404
-app.use((req, res, next) => {
-  res.status(404).send();
-});
 
-module.exports = app;
+app.get('/', (req,res) =>{
+  res.sendFile(path.join(__dirname,'index.html'));
+});
+// for all other routes, return 404
+// app.use((req, res, next) => {
+//   res.status(404).send();
+// });
+
+app.listen(3000);
